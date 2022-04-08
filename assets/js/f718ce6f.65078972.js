823194a5689f0d3cf0bba862202e65e6bebfde59
Yücel Tasyol <ytasyol@agile-is.de>
2022-04-08 14:58:40 +0200

    Deploy website - based on 3a511763fbf7fee82ee98355fd66a1ef3d534748

---
+++ assets/js/f718ce6f.65078972.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkagiletaskboard_docs=self.webpackChunkagiletaskboard_docs||[]).push([[485],{3769:function(s){s.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"default"}}')}}]);
